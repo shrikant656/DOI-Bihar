4da4eb37d52d35093e5c75fff651154ef890d970
shrikant-rolta <shrikant.panigrahi@rolta.com>
2017-07-26 15:33:51 +0530

    Added Template folder

---
+++ template/agriculture-sector/agriculture-sector-controller.js
@@ -0,0 +1,11 @@
+'use strict';
+
+(function(){
+
+    angular.module('routerApp').controller('agricultureController', function ($scope) {
+    	$scope.title="Agriculture Sector";
+
+    })
+    
+
+})()
---
+++ template/register/register-controller.js
@@ -0,0 +1,27 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('routerApp')
+        .controller('RegisterController', ['UserService', '$location', '$rootScope', 'FlashService',function(UserService, $location, $rootScope, FlashService){
+
+        var vm = this;
+
+        vm.register = register;
+
+        function register() {
+            vm.dataLoading = true;
+            UserService.Create(vm.user)
+                .then(function (response) {
+                    if (response.success) {
+                        FlashService.Success('Registration successful', true);
+                        $location.path('/home');
+                    } else {
+                        FlashService.Error(response.message);
+                        vm.dataLoading = false;
+                    }
+                });
+        }
+    }]);
+
+})();
